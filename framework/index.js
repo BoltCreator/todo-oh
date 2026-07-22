e3fdf2f619fca44dcf358c5dbdb64ff9e4d22179
BoltCreator <boltero77@gmail.com>
2026-07-22 01:22:32 +0100

    sync todo/4cacef45 — 2026-07-22T00:22:29.364Z

--- framework/index.js
+++ framework/index.js
@@ -1,2 +1,5 @@
 'use strict';
-module.exports = { BackendApp: require('./BackendApp'), compiler: require('./compiler'), Runner: require('./runner'), util: require('./util') };
+// The bundled framework surface: everything a bundled app can require via
+// require('../framework') or require('.../framework/<module>').
+const points = require('./points');
+module.exports = { BackendApp: require('./BackendApp'), compiler: require('./compiler'), Runner: require('./runner'), util: require('./util'), PointsService: points.PointsService, Ledger: points.Ledger };
